15276e2659ddb6fe0cf0edc22ceedf0006780191
peter fisk <peter.fisk@gmail.com>
2022-12-30 16:55:27 -0500

    show window

--- source/class/vista/Application.js
+++ source/class/vista/Application.js
@@ -1,72 +1,23 @@
-/* ************************************************************************
-
-   Copyright: 2022 undefined
-
-   License: MIT license
-
-   Authors: undefined
-
-************************************************************************ */
-
-/**
- * This is the main application class of "vista"
- *
- * @asset(vista/*)
- */
 qx.Class.define("vista.Application",
-{
-  extend : qx.application.Standalone,
-
-
-
-  /*
-  *****************************************************************************
-     MEMBERS
-  *****************************************************************************
-  */
-
-  members :
-  {
-    /**
-     * This method contains the initial application code and gets called 
-     * during startup of the application
-     * 
-     * @lint ignoreDeprecated(alert)
-     */
-    main : function()
     {
-      // Call super class
-      this.base(arguments);
+        extend: qx.application.Standalone,
 
-      // Enable logging in debug variant
-      if (qx.core.Environment.get("qx.debug"))
-      {
-        // support native logging capabilities, e.g. Firebug for Firefox
-        qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
-        qx.log.appender.Console;
-      }
+        members:
+        {
 
-      /*
-      -------------------------------------------------------------------------
-        Below is your actual application code...
-      -------------------------------------------------------------------------
-      */
+            main: function () {
+                this.base(arguments);
+                const window = new qx.ui.window.Window;
+                window.setCaption('Vista Python');
+                window.setWidth(525);
+                window.setHeight(475);
+                window.addListener('appear', () => { this.onAppear(window); });
+                window.show();
+            },
 
-      // Create a button
-      var button1 = new qx.ui.form.Button("Click me", "vista/test.png");
+            onAppear: function (window) {
+                window.center();
+            }
 
-      // Document is the application root
-      var doc = this.getRoot();
-
-      // Add button to document at fixed coordinates
-      doc.add(button1, {left: 100, top: 50});
-
-      // Add an event listener
-      button1.addListener("execute", function() {
-        /* eslint no-alert: "off" */
-        alert("Hello World!");
-      });
-    }
-  }
-});
+        }
+    });
