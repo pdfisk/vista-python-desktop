c56b8ce27decb67636a7ded2b98fc931bb1e81b1
peter fisk <peter.fisk@gmail.com>
2022-12-31 12:02:07 -0500

    copy resources

--- cp.ps1
+++ cp.ps1
@@ -2,3 +2,4 @@ rm ../site/desktop/resource -Force -Recurse -ErrorAction Ignore
 rm ../site/desktop/vista -Force -Recurse -ErrorAction Ignore
 rm ../site/desktop/index.html -Force -ErrorAction Ignore
 Copy-Item -Path "compiled/dist/vista/*" -Destination "../site/desktop" -Recurse -Force -ErrorAction Ignore
+Copy-Item -Path "source/resource/vista" -Destination "../site/desktop/resource" -Recurse -Force -ErrorAction Ignore
