aa01e1141a55d96b00e445322e7957dfb9a74d1a
peter fisk <peter.fisk@gmail.com>
2023-01-04 15:13:03 -0500

    copy to site

--- cp.ps1
+++ cp.ps1
@@ -1,5 +1,9 @@
 rm ../site/desktop/resource -Force -Recurse -ErrorAction Ignore
 rm ../site/desktop/vista -Force -Recurse -ErrorAction Ignore
 rm ../site/desktop/index.html -Force -ErrorAction Ignore
-Copy-Item -Path "compiled/dist/vista/*" -Destination "../site/desktop" -Recurse -Force -ErrorAction Ignore
+New-Item -ItemType File -Path ../site/desktop/vista/index.js -Force | Out-Null
+Copy-Item -Path "compiled/dist/vista/index.html" -Destination "../site/desktop" -Force -ErrorAction Ignore
+Copy-Item -Path "compiled/dist/vista/resource/qx" -Destination "../site/desktop/resource/qx" -Recurse -Force -ErrorAction Ignore
+Copy-Item -Path "compiled/dist/vista/vista/index.js" -Destination "../site/desktop/vista/index.js" -Force -ErrorAction Ignore
+Copy-Item -Path "source/resource/vista" -Destination "../site/desktop/resource" -Recurse -Force -ErrorAction Ignore
 Copy-Item -Path "source/resource/vista" -Destination "../site/desktop/resource" -Recurse -Force -ErrorAction Ignore
